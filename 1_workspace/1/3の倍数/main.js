53631d9c846407cbae0c7090c9596e56cadd7d6a
YasunoriTomioka <m.h0812y.t@gmail.com>
2021-01-14 16:53:03 +0900

    ３の倍数、fizzbuzz ３がすき、元気な秒針終わりました！

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* ----- ３の倍数
 const genkiFunction = function(number){
   for(let n=1; n<=number; n++){
     if(n % 3 === 0){
@@ -7,3 +9,59 @@ const genkiFunction = function(number){
     }
   }
 }
+*/
+
+/* ----- FizzBuzz
+
+const fbFunction = function(num){
+  for(let n=1; n<=num; n++){
+    if(n%15===0){
+      console.log('FizzBuzz')
+    }else if(n%3===0){
+      console.log('Fizz')
+    }else if(n%5===0){
+      console.log('Buzz')
+    }else{
+      console.log(n)
+    }
+  }
+}
+*/
+
+/* ----- ３が好き
+
+const LoveSan = function(num){
+  for(let n=1; n<=num; n++){
+    if(n % 3 === 0 || String(n).indexOf("3") != -1){
+      console.log(`${n}!!!!!!!`)
+    }else{
+      console.log(n)
+    }
+  }
+}
+*/
+
+/*元気な秒針
+var n = 0
+const judge = function(num){
+  n=num
+  setInterval(clockFunction,1000);
+}
+var i =0
+const clockFunction = function(){
+   i++
+   if(i<=24){
+    if(i % 3 === 0){
+      console.log(`${i}!!!!!!!`)
+    }else{
+      console.log(i)
+    }
+  }
+}
+  
+*/
+
+
+
+
+
